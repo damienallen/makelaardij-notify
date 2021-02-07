5680558092909b3346d71b9c17505b7527ef4775
Damien Allen <contact@dallen.co>
2021-02-07 12:53:13 +0000

    Added prettier

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,22 +3,16 @@ import ReactDOM from 'react-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
 
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import { App } from './App'
 import { theme } from './theme'
 import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-      <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
--- client/src/theme.tsx
+++ client/src/theme.tsx
@@ -3,18 +3,18 @@ import { createMuiTheme } from '@material-ui/core/styles'
 
 // A custom theme for this app
 export const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            default: '#fff',
+        },
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
 })
