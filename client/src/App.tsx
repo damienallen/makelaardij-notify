0b29e3b8c7e03b95e548c56ed7e9fd148410a1bf
Damien Allen <contact@dallen.co>
2021-02-07 15:17:21 +0000

    Cleaned up base material-ui setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline'
 
 import { Layout } from './components/Layout'
 import { StoreProvider } from './stores'
@@ -7,7 +6,6 @@ import { StoreProvider } from './stores'
 export const App: React.FC = () => (
     <StoreProvider>
         <div id="App">
-            <CssBaseline />
             <Layout />
         </div>
     </StoreProvider>
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import { ThemeProvider } from '@material-ui/core/styles'
+import { createStyles, Theme, ThemeProvider, makeStyles } from '@material-ui/core/styles'
+import { CssBaseline, Container } from '@material-ui/core'
 
 import { NavBar } from './NavBar'
 import { useStores } from '../stores'
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        appBarSpacer: theme.mixins.toolbar,
+        content: {
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+        },
+        container: {
+            paddingTop: theme.spacing(4),
+            paddingBottom: theme.spacing(4),
+        },
+    })
+)
+
 export const Layout: React.FC = observer(() => {
     const { settings, ui } = useStores()
+    const classes = useStyles()
+
     return (
         <ThemeProvider theme={ui.theme}>
-            {settings.host}
+            <CssBaseline />
+            <main className={classes.content}>
+                <div className={classes.appBarSpacer} />
+                <Container maxWidth="lg" className={classes.container}>
+                    {settings.host}
+                </Container>
+            </main>
             <NavBar />
         </ThemeProvider>
     )
