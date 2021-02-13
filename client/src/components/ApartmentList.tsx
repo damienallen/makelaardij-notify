384edb6a008169f5795d97320c1c7cc252b3443d
Damien Allen <contact@dallen.co>
2021-02-13 23:37:31 +0000

    Secret feature ;)

--- client/src/components/ApartmentList.tsx
+++ client/src/components/ApartmentList.tsx
@@ -21,10 +21,39 @@ export const ApartmentList: React.FC = observer(() => {
     const { apartments } = useStores()
     const classes = useStyles()
 
+    const now = Date.now()
+    const threholds = [
+        { label: 'Today', value: 0 },
+        { label: 'Yesterday', value: 1 },
+        { label: 'This Week', value: 2 },
+        { label: 'Last Week', value: 7 },
+        { label: 'This Month', value: 14 },
+        { label: 'Last Month', value: 30 },
+        { label: 'Last Year', value: 60 },
+        { label: 'Way Back', value: 365 },
+    ]
+
     const numListings = apartments.filteredList.length
-    let listItems = [<ListSubheader className={classes.subheader}>Today</ListSubheader>]
+    let listItems = []
+
     for (let i = 0; i < numListings; i++) {
-        listItems.push(<Listing key={`listing-${i}`} listing={apartments.filteredList[i]} />)
+        const a = apartments.filteredList[i]
+        const listingDate = apartments.getSortDate(a)
+        const timeDiff = (now - listingDate) / 1000 / 60 / 60 / 24
+
+        if (threholds[0] && timeDiff > threholds[0].value) {
+            const nextThreshold = threholds.shift()
+            listItems.push(
+                <ListSubheader
+                    key={`subheader-${nextThreshold!.label}`}
+                    className={classes.subheader}
+                >
+                    {nextThreshold!.label}
+                </ListSubheader>
+            )
+        }
+
+        listItems.push(<Listing key={`listing-${i}`} listing={a} />)
         if (i < numListings - 1) {
             listItems.push(<Divider key={`divider-${i}`} component="li" />)
         }
