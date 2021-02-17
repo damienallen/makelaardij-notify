6f09a57626d1e5a43b4ee59535c73d1ae4410c71
Damien Allen <contact@dallen.co>
2021-02-17 20:41:46 +0000

    Updated blijdor to new base class

--- server/app/scrapers/blijdorp.py
+++ server/app/scrapers/blijdorp.py
@@ -1,198 +1,170 @@
 import asyncio
-import re
 from datetime import datetime
-from time import sleep
 from typing import List, Union
-
-import httpx
-from app.common import find_float, find_int, get_interval, months_nl, print_new_listing
-from app.models import Apartment
-from bs4 import BeautifulSoup
+from app.common import SkipListing, months_nl
+from app.scrapers.base import BaseScraper
 from odmantic import AIOEngine
 
-BASE_URL = "https://www.blijdorpmakelaardij.nl"
-MAKELAARDIJ = "blijdorp"
-CITY = "rotterdam"
-
-PAGE_DELAY = 2
-LISTING_DELAY = 2
-JITTER = 2
 
 engine = AIOEngine(database="aanbod")
 
-
-async def main():
-    apartment_urls = []
-    skip_limit = 10
-    skip_index = 0
-
-    while skip_index < skip_limit:
-        index_urls = await scrape_page(skip_index)
-        if index_urls:
-            apartment_urls += index_urls
-            sleep(get_interval(PAGE_DELAY, JITTER))
-        else:
-            break
-        skip_index += 1
-
-    print(
-        f"[{datetime.now().isoformat(' ', 'seconds')}] {MAKELAARDIJ}    | Scraped {len(apartment_urls)} listings"
-    )
-
-    for url in apartment_urls:
-        listing = await engine.find_one(Apartment, Apartment.url == f"{BASE_URL}{url}")
-
-        if listing is None:
-            listing_data = await scrape_item(url)
-            apartment = Apartment.parse_obj(listing_data)
-
-            print_new_listing(MAKELAARDIJ, apartment.address)
-            await engine.save(apartment)
-            sleep(get_interval(LISTING_DELAY, JITTER))
+CITY = "rotterdam"
 
 
-async def scrape_page(index: int) -> List[str]:
-    url = f"{BASE_URL}/woningaanbod/koop/{CITY}?skip={index*10}"
-    async with httpx.AsyncClient() as client:
-        result = await client.get(url)
+class BlijdorpScraper(BaseScraper):
+    MAKELAARDIJ: str = "blijdorp"
+    BASE_URL: str = "https://www.blijdorpmakelaardij.nl"
 
-    # Check for good status
-    if result.status_code == 404:
-        return []
-    elif not result.status_code == 200:
-        print(f"Error: {result}")
-        return []
+    # Specific functions
+    async def extract_object_urls(self, soup) -> str:
+        """
+        Extract apartment object urls
+        """
+        urls = []
 
-    # Extract HTML
-    soup = BeautifulSoup(result.content, "html.parser")
-    items = soup.find_all("a", {"class": "object_data_container"})
+        items = soup.find_all("a", {"class": "object_data_container"})
+        for item in items:
+            item_url = item["href"].split("?")[0]
+            urls.append(self.BASE_URL + item_url)
 
-    # Extract apartment object urls
-    urls = []
-    for item in items:
-        item_url = item["href"].split("?")[0]
-        urls.append(item_url)
+        return urls
 
-    return urls
+    async def get_page_url(self, page_num: int) -> str:
+        """
+        Format page url
+        """
+        return f"{self.BASE_URL}/woningaanbod/koop/rotterdam?skip={page_num*10}"
 
+    async def get_apartment_urls(self) -> List[str]:
+        """
+        Fetch list of apartment urls from inventory
+        """
+        urls = []
+        skip_limit = 10
+        skip_index = 0
 
-async def scrape_item(item_url: str):
-    url = f"{BASE_URL}{item_url}"
+        while skip_index < skip_limit:
+            index_urls = await self.scrape_page(skip_index)
+            if index_urls:
+                urls += index_urls
+                self.sleep_interval()
+            else:
+                break
+            skip_index += 1
 
-    async with httpx.AsyncClient() as client:
-        result = await client.get(url)
+        return urls
 
-    # Check for good status
-    if result.status_code == 404:
-        print("Warning, property skipped, not found")
-    elif not result.status_code == 200:
-        raise Exception(f"Error: {result}")
+    def extract_features(self, soup):
+        """
+        Extract feature metadata from listing
+        """
+        meta_data = {
+            "makelaardij": self.MAKELAARDIJ,
+            "building": {},
+            "unit": {"energy": {}, "tags": []},
+            "photos": [],
+        }
 
-    # Extract HTML
-    soup = BeautifulSoup(result.content, "html.parser")
-    photos = soup.find_all("img", {"class": "content"})
-    features = soup.find_all("div", {"class": ["table-responsive", "feautures"]})
+        # Address
+        address = soup.find("h1", {"class": "obj_address"})
+        if address:
+            address_str = (
+                address.string.split(": ")[1]
+                if ": " in address.string
+                else address.string
+            ).split(",")
+        meta_data["address"] = address_str[0]
 
-    item_data = extract_features(features)
-    item_data["url"] = url
+        # Photos
+        photos = soup.find_all("img", {"class": "content"})
+        photo_urls = []
+        for photo in photos:
+            photo_urls.append(photo["src"])
+        meta_data["photos"] = photo_urls
 
-    # Address
-    address = soup.find("h1", {"class": "obj_address"})
-    if address:
-        address_str = (
-            address.string.split(": ")[1] if ": " in address.string else address.string
-        ).split(",")
-    item_data["address"] = address_str[0]
+        # Loop through features
+        features = soup.find_all("div", {"class": ["table-responsive", "feautures"]})
+        raw_data = {}
+        for section in features:
+            table_rows = section.find_all("tr")
+            for row in table_rows:
+                row_data = row.find_all("td")
+                if len(row_data) > 1:
+                    raw_data[row_data[0].string] = row_data[1].string
 
-    # Photos
-    photo_urls = []
-    for photo in photos:
-        photo_urls.append(photo["src"])
-    item_data["photos"] = photo_urls
+        meta_data["added"] = self.find_date(raw_data.get("Aangeboden sinds"))
+        meta_data["updated"] = self.find_date(raw_data.get("Laatste wijziging"))
+        meta_data["asking_price"] = self.find_int(raw_data["Vraagprijs"])
+        meta_data["available"] = (
+            "Status" not in raw_data or raw_data.get("Status") == "Nieuw in verkoop"
+        )
 
-    return item_data
+        meta_data["unit"]["area"] = self.find_float(
+            raw_data["Gebruiksoppervlakte wonen"]
+        )
+        meta_data["unit"]["volume"] = self.find_float(raw_data["Inhoud"])
+        meta_data["unit"]["vve_cost"] = self.find_float(raw_data.get("Servicekosten"))
+        meta_data["unit"]["own_land"] = (
+            raw_data["Eigendom"] == "Eigendom" if raw_data.get("Eigendom") else None
+        )
+        meta_data["unit"]["num_bathrooms"] = self.find_int(raw_data["Aantal badkamers"])
+        meta_data["unit"]["energy"] = {
+            "heating": raw_data.get("Verwarmingssysteem"),
+            "water": raw_data.get("Warm water"),
+            "label": raw_data.get("Energielabel"),
+        }
 
+        meta_data["unit"]["tags"] = self.find_tags(raw_data)
 
-def extract_features(features):
-    """
-    Extract feature metadata from listing
-    """
-    raw_data = {}
-    for section in features:
-        table_rows = section.find_all("tr")
-        for row in table_rows:
-            row_data = row.find_all("td")
-            if len(row_data) > 1:
-                raw_data[row_data[0].string] = row_data[1].string
-
-    # Harder to extract items
-    own_land = raw_data["Eigendom"] == "Eigendom" if raw_data.get("Eigendom") else None
-
-    num_rooms = None
-    if (rooms_str := raw_data.get("Aantal kamers")) is not None:
-        num_rooms = int(rooms_str.split(" ")[0])
-
-    num_floors = None
-    if "Woonlaag" in raw_data and "e woonlaag" in raw_data["Woonlaag"]:
-        num_floors = int(raw_data["Woonlaag"].split("e")[0])
-
-    tags = find_tags(raw_data)
-
-    return {
-        "makelaardij": MAKELAARDIJ,
-        "asking_price": find_int(raw_data["Vraagprijs"]),
-        "available": "Status" not in raw_data
-        or raw_data.get("Status") == "Nieuw in verkoop",
-        "unit": {
-            "area": find_float(raw_data["Gebruiksoppervlakte wonen"]),
-            "volume": find_float(raw_data["Inhoud"]),
-            "energy": {
-                "heating": raw_data.get("Verwarmingssysteem"),
-                "water": raw_data.get("Warm water"),
-                "label": raw_data.get("Energielabel"),
-            },
-            "vve_cost": find_float(raw_data.get("Servicekosten")),
-            "own_land": own_land,
-            "num_bathrooms": find_int(raw_data["Aantal badkamers"]),
-            "num_rooms": num_rooms,
-            "tags": tags,
-        },
-        "building": {
+        meta_data["building"] = {
             "year_constructed": int(raw_data["Bouwperiode"]),
             "building_type": raw_data.get("Soort bouw"),
             "roof_type": raw_data.get("Type dak"),
             "roof_material": raw_data.get("Dakbedekking"),
-            "num_floors": num_floors,
             "parking": raw_data.get("Parkeergelegenheid"),
-        },
-        "added": find_date(raw_data.get("Aangeboden sinds")),
-        "updated": find_date(raw_data.get("Laatste wijziging")),
-    }
+        }
 
+        # Harder to extract items
+        if (rooms_str := raw_data.get("Aantal kamers")) is not None:
+            meta_data["unit"]["num_rooms"] = int(rooms_str.split(" ")[0])
 
-def find_date(date_str: Union[str, None]) -> Union[datetime, None]:
-    if not date_str:
-        return None
+        if "Woonlaag" in raw_data and "e woonlaag" in raw_data["Woonlaag"]:
+            meta_data["building"]["num_floors"] = int(
+                raw_data["Woonlaag"].split("e")[0]
+            )
 
-    date = date_str.split(" ")
-    return datetime(
-        year=int(date[3]), month=months_nl[date[2].lower()], day=int(date[1])
-    )
+        # Bounce broken listings
+        if not meta_data["unit"].get("area"):
+            raise SkipListing("Unable to find area")
 
+        return meta_data
 
-def find_tags(raw_data) -> List[str]:
-    tags = []
-    if raw_data.get("Tuin aanwezig") == "Ja":
-        tags.append("garden")
-    if raw_data.get("Heeft schuur/berging") == "Ja":
-        tags.append("shed")
-    if raw_data.get("Opstal verzekering") == "Ja":
-        tags.append("theft_insurance")
-    if raw_data.get("Heeft een lift") == "Ja":
-        tags.append("lift")
-    return tags
+    @staticmethod
+    def find_date(date_str: Union[str, None]) -> Union[datetime, None]:
+        if not date_str:
+            return None
+
+        date = date_str.split(" ")
+        return datetime(
+            year=int(date[3]), month=months_nl[date[2].lower()], day=int(date[1])
+        )
+
+    @staticmethod
+    def find_tags(raw_data) -> List[str]:
+        tags = []
+        if raw_data.get("Tuin aanwezig") == "Ja":
+            tags.append("garden")
+        if raw_data.get("Heeft schuur/berging") == "Ja":
+            tags.append("shed")
+        if raw_data.get("Opstal verzekering") == "Ja":
+            tags.append("theft_insurance")
+        if raw_data.get("Heeft een lift") == "Ja":
+            tags.append("lift")
+        return tags
 
 
 if __name__ == "__main__":
+    scraper = BlijdorpScraper()
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(scraper.start())
